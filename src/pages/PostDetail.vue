17ac3d804f00902d9b6d27973b1cf8bdc3d1ca98
e <xchsp@sina.com>
2020-03-06 14:41:27 +0800

    文章详情1

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,16 +1,138 @@
 <template>
-    <div>
-      woshi post detail
-      {{this.$route.params.id}}
-    </div>
+      <div class="mainContent">
+        <div class="title">{{post.title}}</div>
+        <video
+          :poster="post.cover[0].url"
+          class="video"
+          controls="controls"
+          src="https://video.pearvideo.com/mp4/adshort/20191031/cont-1618052-14544566_adpkg-ad_sd.mp4"
+          v-if="post.type==2"
+        ></video>
+        <div class="info" v-if="post.user">{{post.user.nickname}} 2019-10-31</div>
+        <div class="content" v-html="post.content" v-if="post.type!=2"></div>
+        <div class="actionBtn">
+          <div class="like">
+            <span :class="{red: post.has_like}" @click="like" class="iconfont icondianzan"></span>
+            {{post.like_length}}
+          </div>
+          <div class="webchat">
+            <span class="iconfont iconweixin"></span>
+            <a
+              href="https://developers.weixin.qq.com/doc/offiaccount/Getting_Started/Explanation_of_interface_privileges.html"
+            >微信</a>
+          </div>
+        </div>
+        <div class="commentList">
+          <h2 class="title">精彩跟帖</h2>
+          <div v-if="post.comment_length==0">
+            <div>暂无跟帖,抢占沙发</div>
+          </div>
+
+          <div class="more">
+            <div @click="toMoreComments" class="moreComment">更多跟帖</div>
+          </div>
+        </div>
+      </div>
 </template>
 
 <script>
     export default {
-        name: "PostDeatail"
+        name: "PostDeatail",
+        data(){
+          return {
+            post:{},
+            postId:this.$route.params.id
+          }
+        },
+        mounted(){
+              this.$axios({
+                url:'/post/' + this.postId,
+                method:'get'
+              }).then(res=>{
+                  console.log(res)
+                  this.post = res
+              })
+        },
+        methods:{
+          toMoreComments(){
+
+          },
+          like(){
+
+          }
+        }
     }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.mainContent {
+  padding-bottom: 80/@vv;
+  padding-left: 10px;
+  padding-right: 10px;
+  .info {
+    margin: 8/@vv 0;
+  }
+  .content {
+    width: 375/@vv;
+    /deep/ img {
+      width: 100%;
+      height: 200/@vv;
+      object-fit: cover;
+    }
+  }
+  .video {
+    width: 100%;
+    margin: 8/@vv 0;
+  }
+  .actionBtn {
+    padding: 30/@vv 0;
+    display: flex;
+    justify-content: space-around;
+    border-bottom: 5/@vv solid #e4e4e4;
+    color: #333;
+    .like {
+      padding: 6/@vv 18/@vv;
+      text-align: center;
+      background-color: #ddd;
+      border-radius: 14/@vv;
+      font-size: 14px;
+    }
+    .webchat {
+      padding: 6/@vv 12/@vv;
+      text-align: center;
+      background-color: #ddd;
+      border-radius: 14/@vv;
+      font-size: 14px;
+      .iconweixin {
+        color: #00c800;
+      }
+    }
+  }
+  .commentList {
+    padding: 20px 0;
+    .title {
+      text-align: center;
+      font-weight: normal;
+      color: #666;
+      font-size: 20px;
+    }
+  }
+  .more {
+    padding-top: 30px;
+    text-align: center;
+    .moreComment {
+      width: 80px;
+      padding: 4px 14px;
+      margin: 0 auto;
+      border-radius: 14px;
+      font-size: 14px;
+      border: 1px solid #888;
+      color: #666;
+      background-color: #eee;
+    }
+  }
+}
+.red {
+  color: #f00;
+}
 </style>
