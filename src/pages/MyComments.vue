c0c05cb73fb28dbe5459c7afd975f4a926cdad08
e <xchsp@sina.com>
2020-03-09 11:43:44 +0800

    完成我的跟帖功能

--- src/pages/MyComments.vue
+++ src/pages/MyComments.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-      我的评论
-    </div>
+  <div>
+    <HeaderMiddle title="我的跟帖"></HeaderMiddle>
+    <comment :commentItem="item" :key="index" v-for="(item,index) in comments" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "MyComments"
+import comment from '@/components/comment'
+import HeaderMiddle from '@/components/HeaderMiddle'
+export default {
+  components: {
+    comment,
+    HeaderMiddle
+  },
+  data() {
+    return {
+      comments: []
     }
+  },
+  methods: {
+
+    getComments() {
+      this.$axios({
+        url: '/user_comments',
+        method: 'get'
+      }).then(res => {
+        console.log(res)
+        this.comments = res
+      })
+    }
+  },
+  mounted() {
+      this.getComments()
+  }
+}
 </script>
 
-<style scoped>
-
+<style>
 </style>
